b5816f8a6e32e47280133bd650dfe7fdaab6fbf8
rBurgett <ryan@burgettdev.net>
2023-04-28 00:21:08 -0400

    Exported missing interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
-import { ServicerRewardCalculator } from './servicer-reward-calculator';
+import { ServicerRewardCalculator, ServicerRewardCalculatorParams } from './servicer-reward-calculator';
 import { ServicerRewardServer } from './servicer-reward-server';
+import { PocketNode, RewardData, RewardDataCondensed, RewardParams, SessionData } from './interfaces';
 
 export {
   ServicerRewardCalculator,
+  ServicerRewardCalculatorParams,
   ServicerRewardServer,
+  PocketNode,
+  RewardData,
+  RewardDataCondensed,
+  RewardParams,
+  SessionData,
 };
